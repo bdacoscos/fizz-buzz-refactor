1829ab00e178c610a80ba6866500b5e7f8d29a51
bdacoscos <briana.dacoscos@gmail.com>
2016-09-20 18:33:25 -0700

    submit button working

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
 $(document).ready(function() {
 	console.log("ready freddy!");
 
-//	function to listen for user input
-	function () {};
+//	listen for user input:
+	$('#user-input-form').submit(function(event) {
+		event.preventDefault();
+		console.log('submit function working');
+
+	});
 
 
 //	function to parse input string into integer: parseInt()
-	function () {};
+	
 	
 
 // 	function to run fizzbuzz
-	function runFizzBuzz() {
+	function runFizzBuzz(userNumber) {
 		for (var i = 1; i <= 100; i++) {
+
+
 
 			// if number divided by both 3 and 5
 			if ((i % 3 === 0) && (i % 5 === 0)) {
